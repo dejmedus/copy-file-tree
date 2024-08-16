2a19204b0cb3d0210c641b5a8abdd27f3c2a28f6
Julia <email96123@gmail.com>
2024-08-16 00:21:39 -0600

    add config tests

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,15 +1,25 @@
-import * as assert from 'assert';
+import * as assert from "assert";
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 // import * as myExtension from '../../extension';
 
-suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+suite("Configuration", () => {
+  test("Valid Configuration", () => {
+    const config = vscode.workspace.getConfiguration("copyFileTree");
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
+    assert.ok(config, "Configuration object should be valid");
+    assert.strictEqual(typeof config, "object");
+  });
+
+  test("Get Settings", () => {
+    const config = vscode.workspace.getConfiguration("copyFileTree");
+
+    const ignoreEnvFiles = config.get("ignoreEnvironmentFiles", null);
+    const ignoreGitIgnore = config.get("ignoreGitIgnore", null);
+
+    assert.strictEqual(typeof ignoreEnvFiles, "boolean");
+    assert.strictEqual(typeof ignoreGitIgnore, "boolean");
+  });
 });
