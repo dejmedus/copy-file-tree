f6122dc6bd8cbac1858c8d8f0af9f246854cdc1b
Julia <email96123@gmail.com>
2024-08-16 13:05:42 -0600

    add docs & custom ignored files

--- src/helpers/getSettings.ts
+++ src/helpers/getSettings.ts
@@ -1,29 +1,52 @@
 import * as vscode from "vscode";
 
-export default function getSettings() {
+export default function getSettings(): {
+  /**
+   * Whether to ignore environment files.
+   */
+  ignoreEnvFiles: boolean;
+
+  /**
+   * Whether to ignore files listed in .gitignore.
+   */
+  ignoreGitIgnore: boolean;
+
+  /**
+   * An array of custom files to ignore.
+   */
+  customIgnoredFiles: string[];
+} {
   const config = vscode.workspace.getConfiguration("copyFileTree");
 
   if (!config) {
     vscode.window.showErrorMessage(
       "No configuration found for Copy file tree extension"
     );
-    return { ignoreEnvFiles: true, ignoreGitIgnore: true };
+    return {
+      ignoreEnvFiles: true,
+      ignoreGitIgnore: true,
+      customIgnoredFiles: [],
+    };
   }
 
   const ignoreEnvFiles = config.get("ignoreEnvironmentFiles");
   const ignoreGitIgnore = config.get("ignoreGitIgnore");
-
-  console.log("settings", ignoreEnvFiles, ignoreGitIgnore);
+  const customIgnoredFiles = config.get("customIgnoredFiles");
 
   if (
     typeof ignoreEnvFiles !== "boolean" ||
-    typeof ignoreGitIgnore !== "boolean"
+    typeof ignoreGitIgnore !== "boolean" ||
+    !Array.isArray(customIgnoredFiles)
   ) {
     vscode.window.showErrorMessage(
       "Invalid configuration found for Copy file tree extension"
     );
-    return { ignoreEnvFiles: true, ignoreGitIgnore: true };
+    return {
+      ignoreEnvFiles: true,
+      ignoreGitIgnore: true,
+      customIgnoredFiles: [],
+    };
   }
 
-  return { ignoreEnvFiles, ignoreGitIgnore };
+  return { ignoreEnvFiles, ignoreGitIgnore, customIgnoredFiles };
 }
